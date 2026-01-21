f77e5fd463783abcb92df5fb5714603ffb8cfe89
Hemanth Kumar Rudramuni <hemantkumarr365@gmail.com>
2026-01-21 10:23:40 +0530

    intern work

---
+++ gui/urls.py
@@ -0,0 +1,40 @@
+"""
+URL configuration for gui project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+
+from django.contrib import admin
+from django.urls import path
+from project1.views import SignupPage, project
+from project1.views import led
+from project1.views import counter
+from project1.views import calci
+from project1.views import index
+from project1.views import LoginPage
+from project1.views import singup
+from project1.views import LogoutPage
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("project1/", project, name="project"),
+    path("led/", led, name="led"),
+    path("counter/", counter, name="counter"),
+    path("calci/", calci, name="calci"),
+    path("index/", index, name="index"),
+    path("login/", LoginPage, name="login"),
+    path("signup/", SignupPage, name="signup"),
+    # keep legacy misspelled route for existing links
+    path("singup/", singup, name="singup"),
+    path("logout/", LogoutPage, name="logout"),
+]
---
+++ project1/views.py
@@ -0,0 +1,113 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+
+
+# Create your views here.
+
+def project(request):
+    return render(request, "project.html")
+
+def led(request):
+    return render(request, "led.html")
+
+def counter(request):
+    if(request.method == "POST"):
+        data = request.POST
+        result = data.get('result')
+
+        if result == "":
+            result = 0
+        else:
+            result = int(data.get('result'))
+        if('increment' in request.POST):
+            result += 1
+            return render(request, "counter.html", context={'result': result})
+        if('decrement' in request.POST):
+            result -= 1
+            return render(request, "counter.html", context={'result': result})
+        if('reset' in request.POST):
+            result = 0
+            return render(request, "counter.html", context={'result': result})
+    return render(request, "counter.html")
+        
+def calci(request):
+    if(request.method=="POST"):
+        data=request.POST
+        num1=data.get("num1")
+        num2=data.get("num2")
+        operation=data.get("operation")
+        if num1 =="" or num2=="":
+            result="Please provide both numbers"
+            return render(request, "calci.html", context={"result": result})
+        else:
+            num1=int(data.get("num1"))
+            num2=int(data.get("num2"))
+            if operation=="add":
+                result=num1+num2
+                return render(request, "calci.html", context={"result": result})
+            if operation=="subtract":
+                result=num1-num2
+                return render(request, "calci.html", context={"result": result})
+            if operation=="multiply":
+                result=num1*num2
+                return render(request, "calci.html", context={"result": result})
+            if operation=="divide":
+                if num2==0:
+                    result="Cannot divide by zero"
+                    return render(request, "calci.html", context={"result": result})
+                else:
+                    result=num1/num2
+                    return render(request, "calci.html", context={"result": result})
+    return render(request, "calci.html")
+
+def index(request):
+    return render(request, "index.html")
+
+def LoginPage(request):
+    if request.method=='POST':
+        username=request.POST.get('username')
+        pass1=request.POST.get('pass')
+        user=User.objects.filter(username=username)
+        user=authenticate(request,username=username,password=pass1)
+        if user is not None:
+          login(request,user)
+          return redirect('index')
+        else:
+          result="Password Entered is wrong"
+          return HttpResponse ("Username or Password is incorrect!!!")
+  
+    return render (request,'login.html')
+
+def SignupPage(request):
+    if request.method=='POST':
+        uname=request.POST.get('username')
+        email=request.POST.get('email')
+        pass1=request.POST.get('password1')
+        pass2=request.POST.get('password2')
+
+        if pass1!=pass2:
+            return HttpResponse("Your password and confrom password are not Same!!")
+        
+        else:
+            my_user=User.objects.create_user(uname,email,pass1)
+            my_user.save()
+            return redirect('login')
+        
+    return render (request,'signup.html')
+    
+          
+    
+def singup(request):
+    # compatibility wrapper for URL imports that expect `singup`
+    return SignupPage(request)
+
+def LogoutPage(request):
+    logout(request)
+    return redirect('login')
+
+
+
+
